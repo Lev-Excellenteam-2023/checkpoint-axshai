a59535f02e366d80cf9ea9ee89f3f3858dfec80c
Shai Axelrod <saxelrod@g.jct.ac.il>
2023-08-01 23:02:50 +0300

    INPUT HANDLERS - REFACTOR

--- input_handlers.h
+++ input_handlers.h
@@ -1,6 +1,10 @@
 #ifndef INPUT_HANDLERS_H
 #define INPUT_HANDLERS_H
 #include "school.h"
-Student* parseLine(char* line, int* sClass, int* level);
 
+#define MAX_LINE_LENGTH 150
+#define MAX_FNAME_LENGTH 25
+#define MAX_LNAME_LENGTH 25
+typedef enum Status_E { Success , Fail} Status;
+Student* parseLine(char* line, int* sClass, int* level);
 #endif
