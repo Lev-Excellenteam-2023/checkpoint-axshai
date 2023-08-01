b0d907e9057cd61ac2ce1221ebc875e4891df4b5
Shai Axelrod <saxelrod@g.jct.ac.il>
2023-08-01 23:02:08 +0300

    PARS LINE -ADD INPUT VALIDATION

--- input_handlers.c
+++ input_handlers.c
@@ -1,42 +1,62 @@
 #include "input_handlers.h"
+#include "dataBase.h"
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define MIN_GRADE_2_STAY 64
+
 Student* parseLine(char* line, int* sClass, int* level)
 {
+    BOOL valid = TRUE;
     char* token;
     if (line[strlen(line) - 1] == '\n') {
         line[strlen(line) - 1] = '\0';
     }
-
+    
     Student* newStudent = (Student*)malloc(sizeof(Student));
     if (!newStudent) {
         return NULL;
     }
+    if (!strlen(line)) {
+        valid = FALSE;
+    }
     newStudent->firstName = (char*)malloc(sizeof(Student));
     token = strtok(line, " ");
-    strncpy(newStudent->firstName, token, sizeof(token) - 1);
-    newStudent->firstName[sizeof(newStudent->firstName) - 1] = '\0';
+    strncpy(newStudent->firstName, token, strlen(token));
+    newStudent->firstName[strlen(token)] = '\0';
 
     newStudent->lastName = (char*)malloc(sizeof(Student));
     token = strtok(NULL, " ");
-    strncpy(newStudent->lastName, token, sizeof(token) - 1);
-    newStudent->lastName[sizeof(newStudent->lastName) - 1] = '\0';
+    strncpy(newStudent->lastName, token, strlen(token));
+    newStudent->lastName[strlen(token)] = '\0';
 
     token = strtok(NULL, " ");
     newStudent->phone = atoi(token);
-
+    if(!newStudent->phone || strlen(token) != 10)
+        valid = FALSE;
     token = strtok(NULL, " ");
     *level = atoi(token);
-
+    if (!(0 < *level && *level <= NUM_OF_LEVELS))
+        valid = FALSE;
     token = strtok(NULL, " ");
     *sClass = atoi(token);
-
+    if (!(0 < *sClass && *sClass <= NUM_OF_CLASSES))
+        valid = FALSE;
 
     for (int i = 0; i < NUM_OF_GRADES; i++) {
         token = strtok(NULL, " ");
         newStudent->grades[i] = atoi(token);
+        if (!(0 <= newStudent->grades[i] && newStudent->grades[i] <= 100))
+            valid = FALSE;
+    }
+    if (!valid)
+    {
+        free(newStudent->firstName);
+        free(newStudent->lastName);
+        free(newStudent);
+        newStudent = NULL;
     }
     return newStudent;
 }
